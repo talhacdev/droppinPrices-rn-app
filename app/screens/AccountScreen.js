d8616bbcae42ea32171ba49af2af21ea81981ad5
talhacdev <thecorruptmob@gmail.com>
2022-07-19 20:14:53 +0500

    refactoring profile screen

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import {
   View,
   StyleSheet,
-  FlatList,
   ScrollView,
   Image,
   Text,
@@ -13,101 +12,26 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import auth from '@react-native-firebase/auth';
-import moment from 'moment';
 
 import routes from '../navigation/routes';
 
 import Header from '../components/Header';
-import ProductCardHeader from '../components/ProductCardHeader';
-import ProductCard from '../components/ProductCard';
-import Carousel from '../components/Carousel';
 
 import colors from '../config/colors';
 
 import {connect} from 'react-redux';
-import {UpdateCart, UpdateProducts} from '../redux/actions/AuthActions';
 import fonts from '../config/fonts';
 import images from '../config/images';
 import Button from '../components/Button';
 
 function AccountScreen(props) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [productsToBuy, setProductsToBuy] = useState([]);
-  const [productsToBid, setProductsToBid] = useState([]);
   const [user, setUser] = useState({});
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     turnToAuction();
-  //   }, 60000);
-  //   return () => clearInterval(interval);
-  // }, []);
-
   useEffect(() => {
-    let reduxProducts = props.productsValue;
     let reduxUser = props.userValue;
 
-    console.log('reduxUser: ', reduxUser.image);
-
-    let tempProductsToBuy = reduxProducts.filter(i => i.bid == false);
-    let tempProductsToBid = reduxProducts.filter(i => i.bid == true);
-
-    setProductsToBuy(tempProductsToBuy);
-    setProductsToBid(tempProductsToBid);
     setUser(reduxUser);
   }, [props.productsValue]);
-
-  const turnToAuction = () => {
-    let reduxProducts = props.productsValue;
-
-    for (let i = 0; i < reduxProducts.length; i++) {
-      if (reduxProducts[i].bid == false) {
-        var now = moment(new Date());
-        var timestamp = reduxProducts[i].timestamp;
-        var duration = moment.duration(now.diff(timestamp));
-        var hours = duration.asHours();
-        if (hours > 0.3) {
-          reduxProducts[i].auctionId = moment();
-          reduxProducts[i].bid = true;
-          props.updateProducts([...reduxProducts]);
-        }
-      }
-    }
-  };
-
-  const calculateDiscount = item => {
-    return 100 - Math.round((item.price / item.originalPrice) * 100);
-  };
-
-  const onPressLike = item => {
-    let reduxProducts = props.productsValue;
-
-    let index = reduxProducts.indexOf(item);
-    reduxProducts[index].liked = !item.liked;
-
-    props.updateProducts([...reduxProducts]);
-  };
-
-  const onPressAddToCart = item => {
-    let reduxCart = props.cartValue;
-
-    let alreadyAdded = reduxCart.filter(i => i.id == item.id);
-
-    if (alreadyAdded.length >= 1) {
-      alert('Product already added');
-    } else {
-      reduxCart.push(item);
-      props.updateCart([...reduxCart]);
-    }
-  };
-
-  const onPressCard = item => {
-    props.navigation.navigate(routes.PRODUCT_DETAIL, {item});
-  };
-
-  const onPressCarousel = () => {
-    props.navigation.navigate(routes.BIDS);
-  };
 
   const onPressLogout = () => {
     auth()
@@ -255,17 +179,8 @@ const styles = StyleSheet.create({
 
 function mapStateToProps(state) {
   return {
-    productsValue: state.auth.products,
-    cartValue: state.auth.cart,
     userValue: state.auth.user,
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    updateProducts: payload => dispatch(UpdateProducts(payload)),
-    updateCart: payload => dispatch(UpdateCart(payload)),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(AccountScreen);
+export default connect(mapStateToProps, null)(AccountScreen);
